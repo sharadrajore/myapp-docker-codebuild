d7e356cb32a1ddd3757ff3649776b819485022c1
Sharad Rajore <sharad.rajore@zensar.com>
2023-05-17 14:41:56 +0530

    first commit

---
+++ src/main/java/com/zensar/MyappDockerCodebuildApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappDockerCodebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappDockerCodebuildApplication.class, args);
+	}
+
+}
